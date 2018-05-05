a0d81c04d3d41ab35ccd95583eb06f3c62f3f00a
Grzegorz Radomski <rc36@wp.pl>
2018-05-05 04:19:01 +0200

    hello world added

--- src/main.java
+++ src/main.java
@@ -1,6 +1,7 @@
 public class main{
 
-    public static void main (String() args){
+    public static void main(String[] args){
         // write your code here
+        System.out.println("Hello world");
     }
 }
